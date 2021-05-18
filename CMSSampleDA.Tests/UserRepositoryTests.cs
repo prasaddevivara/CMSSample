6c29c28e4293d0dab3468bd4433a092dba54cc69
prasaddevivara <prasaddevivara@gmail.com>
2021-05-18 09:06:28 +0530

    Mock Test Cases added for User

--- CMSSampleDA.Tests/UserRepositoryTests.cs
+++ CMSSampleDA.Tests/UserRepositoryTests.cs
@@ -4,12 +4,91 @@ using CMSSample.DA;
 using CMSSample.DA.Repository;
 using CMSSample.DomainModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace CMSSampleDA.Tests
 {
     [TestClass]
     public class UserRepositoryTests
     {
+
+        [TestMethod]
+        public void Test_GetMockAllUsers()
+        {
+            List<User> usrs = new List<User>() 
+            { 
+                new User{UserId=1, UserName="DVedanth1", Password="Test123$", FirstName="Devi", LastName="Prasad", Email="DVedanth1@gmail.com", Mobile=67890 },
+                new User{UserId=2, UserName="GRam", Password="mock123$", FirstName="Ram", LastName="Grand", Email="GRam@gmail.com", Mobile=67890 }
+            };
+            var mock = new Mock<IUserRepository>();
+            mock.Setup(slf => slf.GetUsers()).Returns(usrs);
+
+            Assert.IsNotNull(usrs);
+        }
+
+        [TestMethod]
+        public void Test_GetMockUserByID()
+        {           
+            var mock = new Mock<IUserRepository>();
+            User usr = new User()
+            { UserId = 1, UserName = "DVedanth1", Password = "Test123$", FirstName = "Devi", LastName = "Prasad", Email = "DVedanth1@gmail.com", Mobile = 67890 };
+            
+            int userID = 1;
+            mock.Setup(slf => slf.GetUserByID(userID)).Returns(usr);
+
+            Assert.IsNotNull(usr);
+        }
+
+        [TestMethod]
+        public void Test_MockInsertUser()
+        {
+            var mock = new Mock<IUserRepository>();
+            User user = new User()
+            {
+                UserName = "VRamana",
+                Password = "mypassword",
+                FirstName = "Ramana",
+                LastName = "Vedantham",
+                Email = "vramana@gmail.com",
+                Mobile = 1234
+            };
+            mock.Setup(slf => slf.InsertUser(user));            
+        }
+
+        [TestMethod]
+        public void Test_MockDeleteUser()
+        {
+            var mock = new Mock<IUserRepository>();
+            User user = new User()
+            {
+                UserId = 4,
+                UserName = "VRamana",
+                Password = "mypassword",
+                FirstName = "Ramana",
+                LastName = "Vedantham",
+                Email = "vramana@gmail.com",
+                Mobile = 1234
+            };
+            mock.Setup(slf => slf.DeleteUser(user));           
+        }
+
+        [TestMethod]
+        public void Test_MockUpdateUser()
+        {
+            var mock = new Mock<IUserRepository>();
+            User user = new User()
+            {
+                UserId = 1,
+                UserName = "DVedanth1",
+                Password = "Test123$",
+                FirstName = "Devi",
+                LastName = "Prasad",
+                Email = "DVedanth1@gmail.com",
+                Mobile = 4321
+            };
+            mock.Setup(slf => slf.DeleteUser(user));
+        }
+
         [TestMethod]
         public void Test_GetAllUsers()
         {
